fecc278c1c8931204273775fd887d05a8606013c
SocketSomeone <socket.someone@gmail.com>
2022-05-02 23:13:22 +0200

    style: linting

--- apps/bot/src/modules/emotions/emotion.command.ts
+++ apps/bot/src/modules/emotions/emotion.command.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@nestjs/common";
-import { Autocomplete, Ctx, Opts, SlashCommand } from "necord";
-import { Client, CommandInteraction, EmbedBuilder } from "discord.js";
-import { EmotionDto } from "./emotion.dto";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Emotion } from "./emotion.entity";
-import { Repository } from "typeorm";
-import { EmotionAutocomplete } from "./emotion.autocomplete";
-import { CommonException } from "@miko/common";
+import { Injectable } from '@nestjs/common';
+import { Autocomplete, Ctx, Opts, SlashCommand } from 'necord';
+import { Client, CommandInteraction, EmbedBuilder } from 'discord.js';
+import { EmotionDto } from './emotion.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Emotion } from './emotion.entity';
+import { Repository } from 'typeorm';
+import { EmotionAutocomplete } from './emotion.autocomplete';
+import { CommonException } from '@miko/common';
 
 @Injectable()
 export class EmotionCommand {
